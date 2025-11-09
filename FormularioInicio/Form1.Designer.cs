128e32f59957260456fbfb11efadd8e6448d7fcf
laura123mendonca <lauracmendonca@yahoo.com.ar>
2025-11-09 15:46:44 -0300

    domingo probar si llego

---
+++ FormularioInicio/Form1.Designer.cs
@@ -0,0 +1,346 @@
+﻿namespace FormularioInicio
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            pctPapeleraDeReciclaje = new PictureBox();
+            pctEmail = new PictureBox();
+            pctCarpetaRed = new PictureBox();
+            pctGaleriaFoto = new PictureBox();
+            pctCalculadora = new PictureBox();
+            pictureBox6 = new PictureBox();
+            pctStart = new PictureBox();
+            pctImagenGoogle = new PictureBox();
+            lblPapeleraReciclaje = new Label();
+            lblEmail = new Label();
+            lblCarpetaDeRed = new Label();
+            lblGaleriaImagenes = new Label();
+            lblImagenGoogle = new Label();
+            lblCalculadora = new Label();
+            pnlPapeleraReciclaje = new Panel();
+            pnlCalculadora = new Panel();
+            pnlcarpetaRed = new Panel();
+            panel4 = new Panel();
+            pnlEmail = new Panel();
+            pnlGaleriaImagen = new Panel();
+            pnlImagenGoogle = new Panel();
+            lblBienvenidos = new Label();
+            ((System.ComponentModel.ISupportInitialize)pctPapeleraDeReciclaje).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pctEmail).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pctCarpetaRed).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pctGaleriaFoto).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pctCalculadora).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox6).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pctStart).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pctImagenGoogle).BeginInit();
+            pnlPapeleraReciclaje.SuspendLayout();
+            pnlCalculadora.SuspendLayout();
+            pnlcarpetaRed.SuspendLayout();
+            pnlEmail.SuspendLayout();
+            pnlGaleriaImagen.SuspendLayout();
+            pnlImagenGoogle.SuspendLayout();
+            SuspendLayout();
+            // 
+            // pctPapeleraDeReciclaje
+            // 
+            pctPapeleraDeReciclaje.Image = Properties.Resources.papelera_reciclaje;
+            pctPapeleraDeReciclaje.Location = new Point(20, 14);
+            pctPapeleraDeReciclaje.Name = "pctPapeleraDeReciclaje";
+            pctPapeleraDeReciclaje.Size = new Size(120, 84);
+            pctPapeleraDeReciclaje.SizeMode = PictureBoxSizeMode.StretchImage;
+            pctPapeleraDeReciclaje.TabIndex = 0;
+            pctPapeleraDeReciclaje.TabStop = false;
+            // 
+            // pctEmail
+            // 
+            pctEmail.Image = Properties.Resources.icono_de_email_;
+            pctEmail.Location = new Point(22, 12);
+            pctEmail.Name = "pctEmail";
+            pctEmail.Size = new Size(115, 84);
+            pctEmail.SizeMode = PictureBoxSizeMode.StretchImage;
+            pctEmail.TabIndex = 1;
+            pctEmail.TabStop = false;
+            // 
+            // pctCarpetaRed
+            // 
+            pctCarpetaRed.Image = Properties.Resources.icono_de_carpeta;
+            pctCarpetaRed.Location = new Point(29, 11);
+            pctCarpetaRed.Name = "pctCarpetaRed";
+            pctCarpetaRed.Size = new Size(115, 84);
+            pctCarpetaRed.SizeMode = PictureBoxSizeMode.StretchImage;
+            pctCarpetaRed.TabIndex = 2;
+            pctCarpetaRed.TabStop = false;
+            // 
+            // pctGaleriaFoto
+            // 
+            pctGaleriaFoto.Image = Properties.Resources.icono_galeria_fotos_;
+            pctGaleriaFoto.Location = new Point(27, 27);
+            pctGaleriaFoto.Name = "pctGaleriaFoto";
+            pctGaleriaFoto.Size = new Size(120, 84);
+            pctGaleriaFoto.SizeMode = PictureBoxSizeMode.StretchImage;
+            pctGaleriaFoto.TabIndex = 3;
+            pctGaleriaFoto.TabStop = false;
+            // 
+            // pctCalculadora
+            // 
+            pctCalculadora.Image = Properties.Resources.iconos_calculadoras;
+            pctCalculadora.Location = new Point(25, 18);
+            pctCalculadora.Name = "pctCalculadora";
+            pctCalculadora.Size = new Size(114, 84);
+            pctCalculadora.SizeMode = PictureBoxSizeMode.StretchImage;
+            pctCalculadora.TabIndex = 4;
+            pctCalculadora.TabStop = false;
+            // 
+            // pictureBox6
+            // 
+            pictureBox6.Location = new Point(373, 338);
+            pictureBox6.Name = "pictureBox6";
+            pictureBox6.Size = new Size(8, 8);
+            pictureBox6.TabIndex = 5;
+            pictureBox6.TabStop = false;
+            // 
+            // pctStart
+            // 
+            pctStart.Image = Properties.Resources.start;
+            pctStart.Location = new Point(679, 676);
+            pctStart.Name = "pctStart";
+            pctStart.Size = new Size(551, 289);
+            pctStart.SizeMode = PictureBoxSizeMode.StretchImage;
+            pctStart.TabIndex = 6;
+            pctStart.TabStop = false;
+            // 
+            // pctImagenGoogle
+            // 
+            pctImagenGoogle.Image = Properties.Resources.imagen_de_google;
+            pctImagenGoogle.Location = new Point(17, 14);
+            pctImagenGoogle.Name = "pctImagenGoogle";
+            pctImagenGoogle.Size = new Size(115, 84);
+            pctImagenGoogle.SizeMode = PictureBoxSizeMode.StretchImage;
+            pctImagenGoogle.TabIndex = 7;
+            pctImagenGoogle.TabStop = false;
+            // 
+            // lblPapeleraReciclaje
+            // 
+            lblPapeleraReciclaje.AutoSize = true;
+            lblPapeleraReciclaje.Location = new Point(31, 101);
+            lblPapeleraReciclaje.Name = "lblPapeleraReciclaje";
+            lblPapeleraReciclaje.Size = new Size(87, 40);
+            lblPapeleraReciclaje.TabIndex = 8;
+            lblPapeleraReciclaje.Text = "Papelera de\r\n Reciclaje";
+            // 
+            // lblEmail
+            // 
+            lblEmail.AutoSize = true;
+            lblEmail.Location = new Point(57, 99);
+            lblEmail.Name = "lblEmail";
+            lblEmail.Size = new Size(46, 20);
+            lblEmail.TabIndex = 9;
+            lblEmail.Text = "Email";
+            // 
+            // lblCarpetaDeRed
+            // 
+            lblCarpetaDeRed.AutoSize = true;
+            lblCarpetaDeRed.Location = new Point(43, 98);
+            lblCarpetaDeRed.Name = "lblCarpetaDeRed";
+            lblCarpetaDeRed.Size = new Size(82, 40);
+            lblCarpetaDeRed.TabIndex = 10;
+            lblCarpetaDeRed.Text = "Carpeta de\r\n Red";
+            // 
+            // lblGaleriaImagenes
+            // 
+            lblGaleriaImagenes.AutoSize = true;
+            lblGaleriaImagenes.Location = new Point(48, 131);
+            lblGaleriaImagenes.Name = "lblGaleriaImagenes";
+            lblGaleriaImagenes.Size = new Size(77, 40);
+            lblGaleriaImagenes.TabIndex = 11;
+            lblGaleriaImagenes.Text = "Galeria de\r\n Imagenes";
+            // 
+            // lblImagenGoogle
+            // 
+            lblImagenGoogle.AutoSize = true;
+            lblImagenGoogle.Location = new Point(40, 120);
+            lblImagenGoogle.Name = "lblImagenGoogle";
+            lblImagenGoogle.Size = new Size(58, 20);
+            lblImagenGoogle.TabIndex = 12;
+            lblImagenGoogle.Text = "Google";
+            // 
+            // lblCalculadora
+            // 
+            lblCalculadora.AutoSize = true;
+            lblCalculadora.Location = new Point(35, 105);
+            lblCalculadora.Name = "lblCalculadora";
+            lblCalculadora.Size = new Size(88, 20);
+            lblCalculadora.TabIndex = 13;
+            lblCalculadora.Text = "Calculadora";
+            // 
+            // pnlPapeleraReciclaje
+            // 
+            pnlPapeleraReciclaje.BackColor = Color.Transparent;
+            pnlPapeleraReciclaje.Controls.Add(pctPapeleraDeReciclaje);
+            pnlPapeleraReciclaje.Controls.Add(lblPapeleraReciclaje);
+            pnlPapeleraReciclaje.Location = new Point(25, 23);
+            pnlPapeleraReciclaje.Name = "pnlPapeleraReciclaje";
+            pnlPapeleraReciclaje.Size = new Size(169, 154);
+            pnlPapeleraReciclaje.TabIndex = 14;
+            // 
+            // pnlCalculadora
+            // 
+            pnlCalculadora.BackColor = Color.Transparent;
+            pnlCalculadora.Controls.Add(pctCalculadora);
+            pnlCalculadora.Controls.Add(lblCalculadora);
+            pnlCalculadora.Location = new Point(27, 214);
+            pnlCalculadora.Name = "pnlCalculadora";
+            pnlCalculadora.Size = new Size(167, 146);
+            pnlCalculadora.TabIndex = 15;
+            // 
+            // pnlcarpetaRed
+            // 
+            pnlcarpetaRed.BackColor = Color.Transparent;
+            pnlcarpetaRed.Controls.Add(pctCarpetaRed);
+            pnlcarpetaRed.Controls.Add(lblCarpetaDeRed);
+            pnlcarpetaRed.Location = new Point(211, 214);
+            pnlcarpetaRed.Name = "pnlcarpetaRed";
+            pnlcarpetaRed.Size = new Size(170, 166);
+            pnlcarpetaRed.TabIndex = 16;
+            // 
+            // panel4
+            // 
+            panel4.Location = new Point(513, 574);
+            panel4.Name = "panel4";
+            panel4.Size = new Size(8, 8);
+            panel4.TabIndex = 17;
+            // 
+            // pnlEmail
+            // 
+            pnlEmail.BackColor = Color.Transparent;
+            pnlEmail.Controls.Add(pctEmail);
+            pnlEmail.Controls.Add(lblEmail);
+            pnlEmail.Location = new Point(212, 28);
+            pnlEmail.Name = "pnlEmail";
+            pnlEmail.Size = new Size(171, 149);
+            pnlEmail.TabIndex = 18;
+            // 
+            // pnlGaleriaImagen
+            // 
+            pnlGaleriaImagen.BackColor = Color.Transparent;
+            pnlGaleriaImagen.Controls.Add(pctGaleriaFoto);
+            pnlGaleriaImagen.Controls.Add(lblGaleriaImagenes);
+            pnlGaleriaImagen.Location = new Point(25, 412);
+            pnlGaleriaImagen.Name = "pnlGaleriaImagen";
+            pnlGaleriaImagen.Size = new Size(185, 188);
+            pnlGaleriaImagen.TabIndex = 19;
+            // 
+            // pnlImagenGoogle
+            // 
+            pnlImagenGoogle.BackColor = Color.Transparent;
+            pnlImagenGoogle.Controls.Add(pctImagenGoogle);
+            pnlImagenGoogle.Controls.Add(lblImagenGoogle);
+            pnlImagenGoogle.Location = new Point(226, 425);
+            pnlImagenGoogle.Name = "pnlImagenGoogle";
+            pnlImagenGoogle.Size = new Size(155, 175);
+            pnlImagenGoogle.TabIndex = 20;
+            // 
+            // lblBienvenidos
+            // 
+            lblBienvenidos.BackColor = Color.Gold;
+            lblBienvenidos.Font = new Font("Showcard Gothic", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            lblBienvenidos.Location = new Point(479, 73);
+            lblBienvenidos.Name = "lblBienvenidos";
+            lblBienvenidos.Size = new Size(1324, 527);
+            lblBienvenidos.TabIndex = 21;
+            lblBienvenidos.Text = resources.GetString("lblBienvenidos.Text");
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackgroundImage = Properties.Resources.paisaje_ST;
+            BackgroundImageLayout = ImageLayout.Stretch;
+            ClientSize = new Size(1031, 826);
+            Controls.Add(lblBienvenidos);
+            Controls.Add(pnlImagenGoogle);
+            Controls.Add(pnlGaleriaImagen);
+            Controls.Add(pnlEmail);
+            Controls.Add(panel4);
+            Controls.Add(pnlcarpetaRed);
+            Controls.Add(pnlCalculadora);
+            Controls.Add(pnlPapeleraReciclaje);
+            Controls.Add(pctStart);
+            Controls.Add(pictureBox6);
+            Name = "Form1";
+            Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)pctPapeleraDeReciclaje).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pctEmail).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pctCarpetaRed).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pctGaleriaFoto).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pctCalculadora).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox6).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pctStart).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pctImagenGoogle).EndInit();
+            pnlPapeleraReciclaje.ResumeLayout(false);
+            pnlPapeleraReciclaje.PerformLayout();
+            pnlCalculadora.ResumeLayout(false);
+            pnlCalculadora.PerformLayout();
+            pnlcarpetaRed.ResumeLayout(false);
+            pnlcarpetaRed.PerformLayout();
+            pnlEmail.ResumeLayout(false);
+            pnlEmail.PerformLayout();
+            pnlGaleriaImagen.ResumeLayout(false);
+            pnlGaleriaImagen.PerformLayout();
+            pnlImagenGoogle.ResumeLayout(false);
+            pnlImagenGoogle.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private PictureBox pctPapeleraDeReciclaje;
+        private PictureBox pctEmail;
+        private PictureBox pctCarpetaRed;
+        private PictureBox pctGaleriaFoto;
+        private PictureBox pctCalculadora;
+        private PictureBox pictureBox6;
+        private PictureBox pctStart;
+        private PictureBox pctImagenGoogle;
+        private Label lblPapeleraReciclaje;
+        private Label lblEmail;
+        private Label lblCarpetaDeRed;
+        private Label lblGaleriaImagenes;
+        private Label lblImagenGoogle;
+        private Label lblCalculadora;
+        private Panel pnlPapeleraReciclaje;
+        private Panel pnlCalculadora;
+        private Panel pnlcarpetaRed;
+        private Panel panel4;
+        private Panel pnlEmail;
+        private Panel pnlGaleriaImagen;
+        private Panel pnlImagenGoogle;
+        private Label lblBienvenidos;
+    }
+}
